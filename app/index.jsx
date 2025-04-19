133253fc75cd61d4b1008e0406e760642d2c8639
Vincent Joy <vincentjoyhere@gmail.com>
2025-04-19 14:50:17 +0530

    completed the menu page design and added that navigation to home screen

--- app/index.jsx
+++ app/index.jsx
@@ -12,11 +12,19 @@ const app = () => {
         style={styles.image}
       >
       <Text style={styles.title}>Coffee Shop</Text>
-      <Link href='/contact' style={{ marginHorizontal: 'auto' }} asChild>
-      <Pressable style={styles.button}>
-        <Text style={styles.buttonText}>Contact Us</Text>
+
+      <Link href='/menu' style={{ marginHorizontal: 'auto' }} asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Our Menu</Text>
         </Pressable>
       </Link>
+
+      <Link href='/contact' style={{ marginHorizontal: 'auto' }} asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Contact Us</Text>
+        </Pressable>
+      </Link>
+
       </ImageBackground>
     </View>
   )
@@ -46,10 +54,12 @@ const styles = StyleSheet.create({
   },
   button: {
     height: 60,
+    width: 150,
     borderRadius: 20,
     justifyContent: 'center',
     backgroundColor: 'rgba(0,0,0,0.75)',
-    padding: 6
+    padding: 6,
+    marginBottom: 50
   },
   buttonText: {
     color: 'white',
--- app/menu.jsx
+++ app/menu.jsx
@@ -12,20 +12,30 @@ export default function MenuScreen() {
 
     const Container = Platform.OS == 'web' ? ScrollView : SafeAreaView;
 
+    const separatorComp = <View style={styles.separator} />
+    const footerComp = <Text style={{ color: theme.text }}>End of Menu</Text>
+
     return (
         <Container>
 
             <FlatList
                 data={MENU_ITEMS}
                 keyExtractor={(item) => item.id.toString() }
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={styles.contentContainer}
+                ItemSeparatorComponent={separatorComp}
+                ListFooterComponent={footerComp}
+                ListFooterComponentStyle={styles.footerComp}
+                ListEmptyComponent={<Text>No items</Text>}
                 renderItem={({ item }) => (
-                    <View>
-                        <View>
-                            <Text>{item.title}</Text>
-                            <Text>{item.description}</Text>
+                    <View style={styles.row}>
+                        <View style={styles.menuTextRow}>
+                            <Text style={[styles.menuItemTitle, styles.menuItemText]}>{item.title}</Text>
+                            <Text style={styles.menuItemText}>{item.description}</Text>
                         </View>
                         <Image
                             source={MENU_IMAGES[item.id - 1]}
+                            style={styles.menuImage}
                         />
                     </View>
                 )}
@@ -37,6 +47,53 @@ export default function MenuScreen() {
 
 function createStyles(theme, colorScheme) {
     return StyleSheet.create({
-
+        contentContainer: {
+            paddingTop: 10,
+            paddingBottom: 20,
+            paddingHorizontal: 12,
+            backgroundColor: theme.background,
+        },
+        separator: {
+            height: 1,
+            backgroundColor: colorScheme === 'dark' ? 'papayawhip' : "#000",
+            width: '50%',
+            maxWidth: 300,
+            marginHorizontal: 'auto',
+            marginBottom: 10,
+        },
+        footerComp: {
+            marginHorizontal: 'auto',
+        },
+        row: {
+            flexDirection: 'row',
+            width: '100%',
+            maxWidth: 600,
+            height: 100,
+            marginBottom: 10,
+            borderStyle: 'solid',
+            borderColor: colorScheme === 'dark' ? 'papayawhip' : '#000',
+            borderWidth: 1,
+            borderRadius: 20,
+            overflow: 'hidden',
+            marginHorizontal: 'auto',
+        },
+        menuTextRow: {
+            width: '65%',
+            paddingTop: 10,
+            paddingLeft: 10,
+            paddingRight: 5,
+            flexGrow: 1,
+        },
+        menuItemTitle: {
+            fontSize: 18,
+            textDecorationLine: 'underline',
+        },
+        menuItemText: {
+            color: theme.text,
+        },
+        menuImage: {
+            width: 100,
+            height: 100,
+        }
     })
 }
